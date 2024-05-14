0b6f30f96edfdad98707c8bcfc82c680a1bcdc4b
S1sakshi <shreyasakshi100@gmail.com>
2024-05-14 20:44:10 +0530

    2D Array: Largest row or column

---
+++ largestRow_or_Column.java
@@ -0,0 +1,58 @@
+package Arrays_and_Strings;
+
+import java.util.Scanner;
+
+public class largestRow_or_Coumn {
+    
+    public static void largest(int arr[][], int n, int m) {
+		
+		int largestRowSum = 0;
+		int largestColumnSum = 0;
+		int row = 0;
+		int column = 0;
+		//to check Row sums
+		for(int i=0; i<n; i++) {
+			int sum = 0;
+			for(int j=0; j<m; j++) {
+				sum = sum+arr[i][j];
+			}
+			if(sum>largestRowSum) {
+				largestRowSum = sum;
+				row = i;
+			}
+		}
+		for(int j=0; j<m; j++) {
+			int sum=0;
+			for(int i=0; i<n; i++) {
+				sum = sum+arr[i][j];
+			}
+			if(sum>largestColumnSum) {
+				largestColumnSum = sum;
+				column = j;
+			}
+		}
+		if(largestRowSum>largestColumnSum) {
+			System.out.println("row "+row+" "+largestRowSum);
+		}
+		else {
+			System.out.println("column "+column+" "+largestColumnSum);
+		}
+	}
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of 1st Array: ");
+		int n = sc.nextInt();
+        System.out.print("Enter size of 2nd Array: ");
+		int m = sc.nextInt();
+		
+		int arr[][] = new int[n][m];
+		System.out.println("Enter array elements: ");
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				arr[i][j]= sc.nextInt();
+			}
+		}
+		largest(arr, n, m);
+	}
+}
